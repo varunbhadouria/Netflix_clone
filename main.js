031dbf7a55dbccb84cf6db6fd724e6cb623288ca
Varun Pratap Singh Bhadouria <varunbhadourialional10@gmail.com>
2023-07-27 14:49:28 +0530

    netflix

---
+++ main.js
@@ -0,0 +1,72 @@
+function show1() {
+    var x1 = document.getElementById("Faq-answer1");
+    if (x1.style.display === "none") {
+      x1.style.display = "inline";
+    } else {
+      x1.style.display = "none";
+    }
+  }
+  
+
+
+
+  function show2() {
+    var x2 = document.getElementById("Faq-answer2");
+    if (x2.style.display === "none") {
+      x2.style.display = "inline";
+    } else {
+      x2.style.display = "none";
+    }
+  }
+
+
+
+
+
+  function show3() {
+    var x3 = document.getElementById("Faq-answer3");
+    if (x3.style.display === "none") {
+      x3.style.display = "inline";
+    } else {
+      x3.style.display = "none";
+    } 
+  }
+
+
+
+
+  function show4() {
+    var x4 = document.getElementById("Faq-answer4");
+    if (x4.style.display === "none") {
+      x4.style.display = "inline";
+    } else {
+      x4.style.display = "none";
+    } 
+    
+    var a=document.getElementsId("ques4-icon");
+    a.style.transform="rotate(20deg)";
+  }
+
+
+
+
+  function show5() {
+    var x5 = document.getElementById("Faq-answer5");
+    if (x5.style.display === "none") {
+      x5.style.display = "inline";
+    } else {
+      x5.style.display = "none";
+    } 
+  }
+
+
+
+
+  function show6() {
+    var x6 = document.getElementById("Faq-answer6");
+    if (x6.style.display === "none") {
+      x6.style.display = "inline";
+    } else {
+      x6.style.display = "none";
+    } 
+  }
